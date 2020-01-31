52021733340b463bf108a4f08abb4eeb8981bf71
Artem2313 <huselnykov@gmail.com>
2020-01-31 10:53:01 +0200

    changed shapes in contactList and destructurized filteredContacts 2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -8,7 +8,7 @@ const Contactlist = ({ onDelete, filtedContacts }) => (
       return (
         <li key={id}>
           {name}: {number}
-          <button type="button" onClick={() => onDelete(contact.id)}>
+          <button type="button" onClick={() => onDelete(id)}>
             Delete
           </button>
         </li>
